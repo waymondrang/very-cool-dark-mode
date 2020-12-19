388013a07efe9b177dfb8d665ed6ad3d28118d25
raymond wang <waymondrang@gmail.com>
2020-12-18 20:50:40 -0800

    code cleanup

--- confetti.js
+++ confetti.js
@@ -11,16 +11,13 @@ function startobserving() {
                         var duration = 15 * 1000;
                         var animationEnd = Date.now() + duration;
                         var skew = 1;
-
                         function randomInRange(min, max) {
                             return Math.random() * (max - min) + min;
                         }
-
                         (function frame() {
                             var timeLeft = animationEnd - Date.now();
                             var ticks = Math.max(200, 500 * (timeLeft / duration));
                             skew = Math.max(0.8, skew - 0.001);
-
                             confetti({
                                 particleCount: 1,
                                 startVelocity: 0,
@@ -35,7 +32,6 @@ function startobserving() {
                                 shapes: ['circle'],
                                 scalar: randomInRange(0.4, 1)
                             });
-
                             if (timeLeft > 0) {
                                 requestAnimationFrame(frame);
                             }
@@ -43,25 +39,19 @@ function startobserving() {
                         return
                         //SNOW END
                     }
-
                     //FIREWORK BEGIN
                     var duration = 1.5 * 1000;
                     var animationEnd = Date.now() + duration;
                     var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
                     function randomInRange(min, max) {
                         return Math.random() * (max - min) + min;
                     }
-
                     var interval = setInterval(function () {
                         var timeLeft = animationEnd - Date.now();
-
                         if (timeLeft <= 0) {
                             return clearInterval(interval);
                         }
-
                         var particleCount = 200 * (timeLeft / duration);
-                        // since particles fall down, start a bit higher than random
                         confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
                         confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
                     }, 240);
@@ -69,7 +59,6 @@ function startobserving() {
                     return;
                 }
             });
-
             //START OBSERVING
             secondsubmitobserver.observe(document, {
                 childList: true,
@@ -79,7 +68,6 @@ function startobserving() {
             return;
         }
     });
-
     //START OBSERVING
     submitobserver.observe(document, {
         childList: true,
